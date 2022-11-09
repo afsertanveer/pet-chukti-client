a6f0f82f0668f06544a5fb6f2ba31fe5ba428ca7
Abul Kawser <uiu.afser@gmail.com>
2022-11-09 20:26:46 +0600

    useTitle Hook, favicon,tailwind with daisy ui component added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold ">Hello world!</h1>
+    <div className="max-w-screen-xl mx-auto">
+    
     </div>
   );
 }
