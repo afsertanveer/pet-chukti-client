aaa42ae58464203b2e0268ea61a0eeb28d138065
Abul Kawser <uiu.afser@gmail.com>
2022-11-10 20:51:38 +0600

    token added for social site

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,18 +1,37 @@
 import React, { useContext } from 'react';
 import { toast } from 'react-hot-toast';
 import { FaGoogle } from "react-icons/fa";
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../Context/AuthProvider';
 const SocialLogin = () => {
   const {googleSignIn} = useContext(AuthContext);
   const navigate = useNavigate();
+  
+  const location = useLocation();
+  const from = location.state?.from.pathname || "/";
    const handleGoogleSignIn =  () =>{
     googleSignIn()
     .then(result=>{
       const user = result.user;
+      const currentUser = {
+        email: user?.email,
+      };
       toast.success('Successfully logging in using Google');
-      navigate('/');
+      fetch("https://pet-chukti-server.vercel.app/jwt", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(currentUser),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          localStorage.setItem("token", data.token);
+          navigate(from, { replace: true });
+        });
     })
+    .catch(err=>console.log(err))
    }
     return (
       <div className="text-center">
